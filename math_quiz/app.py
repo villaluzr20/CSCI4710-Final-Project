4c0bb9c3d4d5e3a8e6cfaf13465456f766dd7004
saddHat’git config --global user.email rogersj22@students.ecu.edu’ <rogersj22@students.ecu.edu>
2024-04-14 13:58:39 -0400

    Organized html into templates and png into static. Configured app.py to run a the website and modified the html to properly display pngs

--- math_quiz/app.py
+++ math_quiz/app.py
@@ -2,25 +2,53 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-# Place Database connection and setup code here
+app.static_folder = 'static'
 
 @app.route('/')
-def intro():
-    return render_template('intro.html')
+def main():
+    return render_template('main.html')
 
-@app.route('/study')
+@app.route('/study.html')
 def study():
-    # Logic to fetch questions from database and pass them to template
-    return render_template('study.html', questions=questions)
+    return render_template('study.html')
 
-@app.route('/quiz', methods=['GET', 'POST'])
-def quiz():
-    if request.method == 'POST':
+@app.route('/test.html')
+def test():
+    return render_template('test.html')
+
+@app.route('/addition.html')
+def addition():
+    return render_template('addition.html')
+
+@app.route('/subtraction.html')
+def subtraction():
+    return render_template('subtraction.html')
+
+@app.route('/multiplication.html')
+def multiplication():
+    return render_template('multiplication.html')
+
+@app.route('/division.html')
+def division():
+    return render_template('division.html')
+
+
+# Place Database connection and setup code here
+
+
+#@app.route('/study')
+#def study():
+    # Logic to fetch questions from database and pass them to template
+    #return render_template('study.html', questions=questions)
+
+#@app.route('/quiz', methods=['GET', 'POST'])
+#def quiz():
+    #if request.method == 'POST':
         # Logic to calculate score
-        return render_template('score.html', score=score)
-    else:
+        #return render_template('score.html', score=score)
+    #else:
         # Logic to fetch questions from database and pass them to template
-        return render_template('quiz.html', questions=questions)
+        #return render_template('quiz.html', questions=questions)
 
 if __name__ == '__main__':
     app.run(debug=True)
